b7884582006b1f0a3e26a2849f9f127aab5b81c4
Sadman Sakib <sadman.93.sakib@gmail.com>
2020-10-19 15:28:52 -0400

    Create Birthday Cake Candles.py
    Problem URL:
    https://www.hackerrank.com/challenges/birthday-cake-candles/problem

---
+++ Birthday Cake Candles.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    maxNum=max(ar)
+    countMax=ar.count(maxNum)
+    return countMax
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
