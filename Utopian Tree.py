0acb1499d31f641f6335f822d455eacc0259fc62
Sadman Sakib <sadman.93.sakib@gmail.com>
2020-10-23 15:30:21 -0400

    Add files via upload
    Problem URL:
    https://www.hackerrank.com/challenges/utopian-tree/problem

---
+++ Utopian Tree.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the utopianTree function below.
+def utopianTree(n):
+    height=1
+    for cycle in range(0, n+1):
+        if(cycle==0):
+            height=height
+        elif(cycle%2!=0):
+            height+=height
+        elif(cycle%2==0):
+            height+=1
+        # print("height", height)
+    return height
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
