e8bdcd5207b9e9c74c422bf6445884bc04235245
Sadman Sakib <sadman.93.sakib@gmail.com>
2020-10-26 12:49:10 -0400

    Add files via upload
    https://www.hackerrank.com/challenges/extra-long-factorials/problem

---
+++ Extra Long Factorials.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    fact = 1
+    for i in range(1,n+1): 
+        fact = fact * i 
+    print(fact)
+if __name__ == '__main__':
+    n = int(input())
+
+    extraLongFactorials(n)
