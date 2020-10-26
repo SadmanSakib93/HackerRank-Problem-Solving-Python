bc7b39d860653b1e299e510a17709bbdd3658e69
Sadman Sakib <sadman.93.sakib@gmail.com>
2020-10-26 12:47:14 -0400

    Add files via upload
    https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem

---
+++ Jumping on the Clouds Revisited.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    energy = 100 
+    i = k % n 
+    energy -= c[i] * 2 + 1
+    while (i != 0):
+        i = (i + k) % n
+        energy -= c[i] * 2 + 1
+        
+    return energy
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
