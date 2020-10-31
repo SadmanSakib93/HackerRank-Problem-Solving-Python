a7eeec0c7d663e9e1e2b1c8882fbb3a978bccf37
Sadman Sakib <sadman.93.sakib@gmail.com>
2020-10-31 01:11:15 -0400

    Add files via upload
    https://www.hackerrank.com/challenges/beautiful-triplets/problem

---
+++ Beautiful Triplets.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulTriplets function below.
+def beautifulTriplets(d, arr):
+    count=0
+
+    for i in range(len(arr)):
+        if(arr[i]+d in arr and arr[i]+d+d in arr):
+            count+=1
+    
+    return (count)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
