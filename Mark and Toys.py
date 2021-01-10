f6d745fdec93c1955932deafc5e0f9c6db6ca421
Sadman Sakib <sadman.93.sakib@gmail.com>
2021-01-09 21:25:41 -0500

    Add files via upload
    https://www.hackerrank.com/challenges/mark-and-toys/problem

---
+++ Mark and Toys.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    count = 0
+    sumTemp = 0
+    for each in prices:
+        sumTemp += each
+        if(sumTemp <= k):
+            count += 1
+        else:
+            break
+        
+    return count
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
