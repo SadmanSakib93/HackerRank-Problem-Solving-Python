25140472a4556162c5880b316116321cda6d9fa7
Sadman Sakib <sadman.93.sakib@gmail.com>
2021-06-02 05:51:34 -0400

    Add files via upload
    https://www.hackerrank.com/challenges/strange-code/problem

---
+++ Strange Counter.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    # Write your code here
+    base_counter = 3
+    while t > base_counter:
+        t = t-base_counter
+        base_counter *= 2
+    return (base_counter-t+1)
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
