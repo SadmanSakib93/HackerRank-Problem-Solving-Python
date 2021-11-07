beefdc9da0c99808d4f36abb5f45a8bdcdf95417
Sadman Sakib <sadman.93.sakib@gmail.com>
2021-11-07 09:43:29 -0500

    Upload Tower Breakers.py
    https://www.hackerrank.com/challenges/tower-breakers-1/problem

---
+++ Tower Breakers.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    # Write your code here
+    
+    # If all the towers are of height 1, then Player 1 is unable to move in first time, and player 2 wins!
+    if(m==1):
+        return 2
+   
+    towers=[m]*n  
+    if(len(towers)%2==0):
+        return 2
+    else:
+        return 1
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = towerBreakers(n, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
